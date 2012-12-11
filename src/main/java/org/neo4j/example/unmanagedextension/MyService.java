237d333abd6d479f4272e3e82bbf0b4b04deaa22
Volkan Tufekci <volkan.tufekci@crenno.com>
2012-12-11 12:05:21 +0200

    postVolkan is refactored into smaller methods

--- src/main/java/org/neo4j/example/unmanagedextension/MyService.java
+++ src/main/java/org/neo4j/example/unmanagedextension/MyService.java
@@ -15,6 +15,8 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.map.JsonMappingException;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.neo4j.cypher.javacompat.ExecutionEngine;
 import org.neo4j.cypher.javacompat.ExecutionResult;
@@ -37,10 +39,13 @@ public class MyService {
 
     @GET
     @Path("/friends/{name}")
-    public Response getFriends(@PathParam("name") String name, @Context GraphDatabaseService db) throws IOException {
+    public Response getFriends(@PathParam("name") String name, @Context GraphDatabaseService db) 
+    		throws IOException {
         ExecutionEngine executionEngine = new ExecutionEngine(db);
-        ExecutionResult result = executionEngine.execute("START person=node:people(name={n}) MATCH person-[:KNOWS]-other RETURN other.name",
-                Collections.<String, Object>singletonMap("n", name));
+        ExecutionResult result = executionEngine.execute(
+        				"START person=node:people(name={n}) " +
+        				"MATCH person-[:KNOWS]-other RETURN other.name",
+        				Collections.<String, Object>singletonMap("n", name));
         List<String> friends = new ArrayList<String>();
         for (Map<String, Object> item : result) {
             friends.add((String) item.get("other.name"));
@@ -67,38 +72,62 @@ public class MyService {
 	public Response postVolkan(@Context GraphDatabaseService db, InputStream is)
 			throws IOException {
 
-		Map<String, Object> jsonMap	= null;
-		List<String> nodes 			= new ArrayList<String>();
+		Map<String, Object> inputMap	= null;
+		List<String> resultJson 		= new ArrayList<String>();
 
 		ObjectMapper mapper = new ObjectMapper();
 		try {
-			String input = readFromStream(is);
-			jsonMap = mapper.readValue(input, Map.class);
-			int depth = (Integer) jsonMap.get("depth");
-			TraversalDescription traversal = Traversal.description().evaluator(
-					Evaluators.atDepth(depth));
+			inputMap = convertJsonToMap(is, mapper);
+			
+			TraversalDescription traversalDesc = Traversal.description();
+			traversalDesc = addDepth(inputMap, traversalDesc);
+			traversalDesc = addRelationships(inputMap, traversalDesc);
 
-			for (Map<String, String> rel : (List<Map<String, String>>) jsonMap.get("relationships")) {
-				String relationName = rel.get("type");
-				Direction direction = findOutDirection(rel);
-				traversal = traversal.relationships(
-						DynamicRelationshipType.withName(relationName),
-						direction);
-			}
-
-			Node startNode = db.getNodeById((int) jsonMap.get("start_node"));
-
-			for (Node node : traversal.traverse(startNode).nodes()) {
-				String name = (String) node.getProperty("name");
-				nodes.add(name);
-			}
+			resultJson = traverse(db, inputMap, traversalDesc);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
-		return Response.ok().entity(mapper.writeValueAsString(nodes)).build();
+		return Response.ok().entity(mapper.writeValueAsString(resultJson)).build();
+	}
 
-		// return userDataRead.get("client").toString();
+	private List<String> traverse(GraphDatabaseService db, Map<String, Object> inputMap, 
+							TraversalDescription traversal) {
+		List<String> resultJson = new ArrayList<String>();
+		Node startNode = db.getNodeById((int) inputMap.get("start_node"));
+		
+		for (Node node : traversal.traverse(startNode).nodes()) {
+			String name = (String) node.getProperty("name");
+			resultJson.add(name);
+		}
+		
+		return resultJson;
+	}
+
+	private Map<String, Object> convertJsonToMap(InputStream is, ObjectMapper mapper) 
+			throws IOException, JsonParseException, JsonMappingException {
+		Map<String, Object> inputMap;
+		String input 	= readFromStream(is);
+		inputMap 		= mapper.readValue(input, Map.class);
+		return inputMap;
+	}
+
+	private TraversalDescription addDepth(
+				Map<String, Object> jsonMap, TraversalDescription traversal) {
+		int depth = (Integer) jsonMap.get("depth");
+		return traversal.evaluator(Evaluators.atDepth(depth));
+	}
+
+	private TraversalDescription addRelationships(Map<String, Object> jsonMap,
+			TraversalDescription traversal) {
+		for (Map<String, String> rel : (List<Map<String, String>>) jsonMap.get("relationships")) {
+			String relationName = rel.get("type");
+			Direction direction = findOutDirection(rel);
+			traversal = traversal.relationships(
+					DynamicRelationshipType.withName(relationName),
+					direction);
+		}
+		return traversal;
 	}
 
 	private String readFromStream(InputStream stream) throws IOException {
@@ -115,7 +144,7 @@ public class MyService {
 		return new String(baos.toByteArray());
 	}
 
-	private static Direction findOutDirection(Map<String, String> rel) {
+	private Direction findOutDirection(Map<String, String> rel) {
 		String directionString = rel.get("direction");
 		Direction direction = null;
 		if (directionString.equalsIgnoreCase("IN")) {
