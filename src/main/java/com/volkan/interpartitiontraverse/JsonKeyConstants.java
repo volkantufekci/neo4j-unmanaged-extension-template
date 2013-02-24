fafc32e7983d48774580006ee19e600d09168214
Volkan Tufekci <volkan.tufekci@crenno.com>
2013-02-24 17:30:41 +0200

    start_port is added

--- src/main/java/com/volkan/interpartitiontraverse/JsonKeyConstants.java
+++ src/main/java/com/volkan/interpartitiontraverse/JsonKeyConstants.java
@@ -6,4 +6,5 @@ public class JsonKeyConstants {
 	public static final String PATH				= "path";
 	public static final String DEPTH			= "depth";
 	public static final String JOB_ID 			= "job_id";
+	public static final String START_PORT		= "start_port";
 }
